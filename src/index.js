dbf2874d475c9b4e6e6b7be321db860ee59e7699
ycespedes <cespedesyavet@gmail.com>
2018-05-25 16:50:36 -0500

    final

--- src/index.js
+++ src/index.js
@@ -18,18 +18,4 @@ function encrypt(){
     };
     return crypted;
    }
-   function ATH(s) { 
-       // Convierte una cadena ASCII to HEX
-     for (var ath="", i=0; i<s.length-1; i+=2) {
-       ath += String.fromCharCode(parseInt(s.substr(i,2),16));
-     }
-     return ath;
-   }
-   
-   function HTA(s) { // Convierte una cadena HEX a ASCII
-     for (var hta="", i=0; i<s.length; i++) {
-       var z = "0"+s.charCodeAt(i).toString(16).toUpperCase();
-       hta += z.substr(z.length-2,2);
-     }
-     return hta;
-   }
+  

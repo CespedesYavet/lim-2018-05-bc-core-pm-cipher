facb5d892109f8b5a1e751e5143b0ee8ae0ae0db
ycespedes <cespedesyavet@gmail.com>
2018-05-30 18:30:30 -0500

    subiendo modificacion

--- src/cipher.js
+++ src/cipher.js
@@ -1,2 +1,14 @@
-window.cipher = {
-};
+/*window.cipher = {
+};*/
+function encode(ingresaPalabraE,numDesplazamiento) {
+    
+    let output = "";
+    for (let i = 0; i < length.length; i++) {
+    output += string.fromCharCode(val1.fromCharCode(i)+ val2);
+    }
+    return output;
+    }
+    
+    function decode(){
+    
+    } 

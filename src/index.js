68a50fe204d7de048d5b3111fb0a9f41b91d8011
ycespedes <cespedesyavet@gmail.com>
2018-06-04 14:35:24 -0500

    terminando con html

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@
     let encode = document.getElementById('cipher');//boton encriptar
     let decode = document.getElementById('decipher');//boton desencriptar
     let result = document.getElementById('result'); //limpiar
-    let salida = document.getElementById('clearbutton').value="";
+    //let salida = document.getElementById('clearbutton').value="";
 
   encode.addEventListener('click', () => {
-    console.log('entre al click');
+    //console.log('entre al click');
 
     let inputWord = input.value;
     let inputnumber = desplazamiento.value;
-    console.log(inputWord);
-    console.log(inputnumber);
-    result.innerHTML = 'El texto desencriptado es <br/>: ' + cipher.encode(inputnumber,inputWord) ;
+    //console.log(inputWord);
+    //console.log(inputnumber);
+    result.innerHTML = 'El texto encriptado es <br/>: ' + cipher.encode(inputnumber,inputWord) ;
   });
 
   decode.addEventListener('click', () => {
